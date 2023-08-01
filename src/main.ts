850e0614346eee02084e38153c251ca06f3b1241
wuyanfeiying <wuyanfeiying@163.com>
2023-08-01 20:13:02 +0800

    perf: commit hook

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+/*
+ * @Date: 2023-08-01 19:43:56
+ * @LastEditors: wuyanfeiying
+ */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+console.log(2);
+a = 123;
 createApp(App).mount('#app')
