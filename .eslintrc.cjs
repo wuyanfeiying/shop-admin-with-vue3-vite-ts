a1ff89c2bce86e3355d5d22245305841c574bfaa
wuyanfeiying <wuyanfeiying@163.com>
2023-08-01 19:47:58 +0800

    eslint 配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,28 @@
+/*
+ * @Date: 2023-07-31 21:32:01
+ * @LastEditors: wuyanfeiying
+ */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "parser": "@typescript-eslint/parser",
-        "extraFileExtensions": [".vue"]
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/vue3-strongly-recommended', 'standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-mutating-props': 'off'
+  },
+  overrides: [
+    {
+      files: ['src/api/**/*.ts'],
+      rules: {
+        camelcase: 'off'
+      }
     }
+  ]
 }
