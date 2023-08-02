30b7f1cdb92542a8c11e6e467473827760e5f3a0
wuyanfeiying <wuyanfeiying@163.com>
2023-08-02 20:00:01 +0800

    perf: 代码规范和ESLint在开发和构建时候的验证

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/*
+ * @Date: 2023-07-31 20:17:42
+ * @LastEditors: wuyanfeiying
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置文件
+      cache: false // 禁用 eslint 缓存
+    })
+  ]
 })
