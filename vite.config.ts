2eddb77aee80467340a260b4aca55ce24c1a8a02
wuyanfeiying <wuyanfeiying@163.com>
2023-08-06 14:28:43 +0800

    perf: 配置转换JSX和TSX

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslintPlugin({
-      // 配置文件
+      // 配置选项
       cache: false // 禁用 eslint 缓存
+    }),
+    vueJsx({
+      // 配置选项
     })
   ]
 })
