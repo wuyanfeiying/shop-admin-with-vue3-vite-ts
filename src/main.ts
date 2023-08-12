8b75d8b873714fe99253f06daf0fd2aa9dbfc70a
wuyanfeiying <wuyanfeiying@163.com>
2023-08-12 11:58:33 +0800

    perf: 安装 element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@
  * @LastEditors: wuyanfeiying
  */
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
+import elementPlus from './plugins/element-plus'
+
+// 加载全局样式
 import './styles/index.scss'
 
 createApp(App)
   .use(router)
   .use(store, key)
+  .use(elementPlus)
   .mount('#app')
---
+++ src/plugins/element-plus.ts
@@ -0,0 +1,15 @@
+/*
+ * @Date: 2023-08-12 11:29:24
+ * @LastEditors: wuyanfeiying
+ * @desc: element plus 第三方UI组件
+ */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { App } from 'vue'
+
+export default {
+  install (app:App) {
+    app.use(ElementPlus, { size: 'small', zIndex: 2000, locale: zhCn })
+  }
+}
