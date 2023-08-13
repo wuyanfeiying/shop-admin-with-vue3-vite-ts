2690475b5183ac5108b66f5046eac8d8abe32ece
wuyanfeiying <wuyanfeiying@163.com>
2023-08-13 16:40:36 +0800

    perf: 页面加载进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,15 @@
  * @Date: 2023-08-06 14:34:44
  * @LastEditors: wuyanfeiying
  */
+import nprogress from 'nprogress'
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import AppLayout from '@/layout/AppLayout.vue'
 import mediaRoutes from './modules/media'
 import orderRoutes from './modules/order'
 import permissionRoutes from './modules/permission'
 import productRoutes from './modules/product'
+
+import 'nprogress/nprogress.css'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -42,4 +45,11 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach(() => {
+  nprogress.start() // 开始加载进度条
+})
+
+router.afterEach(() => {
+  nprogress.done() // 结束进度条
+})
 export default router
