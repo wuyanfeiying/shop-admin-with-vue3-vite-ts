6c59759e2423b0c29016eed7f7d7a6c0fc7020b2
wuyanfeiying <wuyanfeiying@163.com>
2023-08-19 13:53:22 +0800

    perf: 管理员列表-列表分页

--- src/views/permission/admin/index.vue
+++ src/views/permission/admin/index.vue
@@ -3,13 +3,159 @@
  * @LastEditors: wuyanfeiying
 -->
 <template>
-  admin
+  <page-container>
+    <app-card>
+      <template #header>
+        数据筛选
+      </template>
+      <el-form
+        :inline="true"
+        ref="form"
+        :model="listParams"
+        @submit.prevent="handleQuery"
+      >
+        <el-form-item label="状态">
+          <el-select
+            v-model="listParams.status"
+            placeholder="请选择"
+            clearable
+          >
+            <el-option
+              label="全部"
+              value=""
+            />
+            <el-option
+              label="显示"
+              :value="1"
+            />
+            <el-option
+              label="不显示"
+              :value="0"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="管理员名称">
+          <el-input
+            v-model="listParams.name"
+            clearable
+            placeholder="请输入身份昵称"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button native-type="submit">
+            查询
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </app-card>
+    <app-card>
+      <template #header>
+        <el-button
+          type="primary"
+        >
+          添加管理员
+        </el-button>
+      </template>
+      <el-table
+        :data="list"
+        stripe
+        style="width: 100%"
+        v-loading="listLoading"
+      >
+        <el-table-column
+          prop="id"
+          label="ID"
+        />
+        <el-table-column
+          prop="real_name"
+          label="姓名"
+        />
+        <el-table-column
+          prop="account"
+          label="账号"
+        />
+        <el-table-column
+          prop="roles"
+          label="身份"
+          min-width="180"
+        >
+          <template #default="scope">
+            <el-space wrap>
+              <el-tag
+                class="role-tag"
+                v-for="(item) in scope.row.roles.split(',')"
+                :key="item"
+              >
+                {{ item }}
+              </el-tag>
+            </el-space>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="最后一次登录时间"
+          prop="last_time"
+        />
+        <el-table-column
+          label="最后一次登录IP"
+          prop="last_ip"
+        />
+        <el-table-column label="状态">
+          <template #default="scope">
+            <el-switch
+              v-model="scope.row.status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              :active-value="1"
+              :inactive-value="0"
+              :loading="scope.row.statusLoading"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          fixed="right"
+          min-width="100"
+          align="center"
+        />
+      </el-table>
+      <app-pagination
+        v-model:page="listParams.page"
+        v-model:limit="listParams.limit"
+        :list-count="listCount"
+      />
+    </app-card>
+  </page-container>
 </template>
 
-<script setup lang='ts'>
+<script lang="ts" setup>
+import { getAdmins } from '@/api/admin'
+import type { Admin, IListParams } from '@/api/types/admin'
+import { onMounted, reactive, ref } from 'vue'
+
+const list = ref<Admin[]>([]) // 列表数据
+const listCount = ref(0)
+const listLoading = ref(true)
+const listParams = reactive({ // 列表数据查询参数
+  page: 1, // 当前页码
+  limit: 10, // 每页大小
+  name: '',
+  roles: '',
+  status: '' as IListParams['status'] // 显性的告诉这个是什么类型,listparams里面声明了一个联合类型，其中包括字符串
+})
+
+onMounted(() => {
+  loadList()
+})
+
+const loadList = async () => {
+  const data = await getAdmins(listParams)
+  list.value = data.list
+}
+
+const handleQuery = async () => {
+  loadList()
+}
 
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped></style>
